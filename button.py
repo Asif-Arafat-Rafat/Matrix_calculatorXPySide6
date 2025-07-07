b83ecd43cedbd2928b42f3a81512b717b11d460d
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-07-07 12:13:28 +0600

    All Signal are Demostrated v0.0.1

--- button.py
+++ button.py
@@ -1,11 +1,22 @@
 from PySide6.QtWidgets import QMainWindow ,QApplication ,QPushButton
 def button_clicked():
     print("Clicked")
+def button_released():
+    print("Released")
+def button_pressed():
+    print("Pressed")
+def button_toggled(checked):
+    print("Toggled",checked)
 class Buttonholder(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Buttonholder")
         button=QPushButton("Press Me")
+        button.setCheckable(True)
         button.clicked.connect(button_clicked)
+        button.released.connect(button_released)
+        button.pressed.connect(button_pressed)
+        button.toggled.connect(button_toggled)
+        # The order : Pressed,Toggled,Released,Clicked
         self.setCentralWidget(button)
 
