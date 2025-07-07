d95e3112e01504cca870fbd236c91807e45d5bad
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-07-07 11:57:07 +0600

    Button separated and connected v0.0.1

--- button.py
+++ button.py
@@ -1,7 +1,11 @@
 from PySide6.QtWidgets import QMainWindow ,QApplication ,QPushButton
+def button_clicked():
+    print("Clicked")
 class Buttonholder(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Buttonholder")
         button=QPushButton("Press Me")
+        button.clicked.connect(button_clicked)
         self.setCentralWidget(button)
+
